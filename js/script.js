b75556bf82531371a95562eae2ccb32dc4cd9682
Michał Kukuła <michal13171@wp.pl>
2018-02-13 18:18:52 +0100

    ini

--- js/script.js
+++ js/script.js
@@ -1,45 +1,52 @@
 'use strict';
 $(function () {
     $('.two').on("click", watchimage);
-    $('.one').on('click',function (e) {
-        e.preventDefault();
-        $('.first-element-image').hide();
-        $('.two-element-image').hide();
-        $('.image').show();
-
-    });
+    $('.one').on('click', hideoneClass);
 
     function watchimage(e) {
         e.preventDefault();
-        var imageTab = ['img/portfoliophoto6.png', 'img/portfoliophoto3.png', 'img/portfolio%20photo.png', 'img/portfoliophoto1.png'];
-        var imageTab2 = ['img/portfoliophoto4.png', 'img/portfoliophoto5.png', 'img/portfoliophoto7.png', 'img/portfolio%20photo2.png'];
 
-        $('.image').hide();
         $('.elements-image').hide();
-        $('.first-element-image').css({"display":"flex","justify-content":"space-between","align-items":"center"});
-        $('.two-element-image').css({"display":"flex","justify-content":"space-between","align-items":"center"});
+        $('.first-element-image').css({"display": "flex", "justify-content": "space-between", "align-items": "center"});
+        $('.two-element-image').css({"display": "flex", "justify-content": "space-between", "align-items": "center"});
 
         $('.first-element-image').show();
         $('.two-element-image').show();
 
+        var imageTab = ['img/portfoliophoto6.png', 'img/portfoliophoto3.png', 'img/portfolio%20photo.png', 'img/portfoliophoto1.png'];
+        var imageTabSecond = ['img/portfoliophoto4.png', 'img/portfoliophoto5.png', 'img/portfoliophoto7.png', 'img/portfolio%20photo2.png'];
+
         var container = $('.first-element-image');
         var container1 = $('.two-element-image');
-       // var container = $('.elements-image');
 
+        var addImg1 = $('.image1');
+        var addImg2 = $('.image2');
+        var addImg3 = $('.image3');
+        var addImg4 = $('.image4');
         for (let i = 0, j = imageTab.length; i < j; i++) {
-
             let img = document.createElement('img');
             img.src = imageTab[i]; // img[i] refers to the current URL.
             container.append(img);
-
         }
-        for (let i = 0, j = imageTab2.length; i < j; i++) {
+        // forEach (const i; in imageTabSecond.length) {
+        //     var attr = imageTabSecond[i]
+        //     container1.html('<img src="' + attr + '">');
+        //     console.log('<img src="' + attr + '">');
+        //     console.log(attr[i]);
+        // }
 
-            let img = document.createElement('img');
-            img.src = imageTab2[i]; // img[i] refers to the current URL.
-            container1.append(img);
+            addImg1.attr("src",imageTabSecond[0]);
+            addImg2.attr("src",imageTabSecond[1]);
+            addImg3.attr("src",imageTabSecond[2]);
+            addImg4.attr("src",imageTabSecond[3]);
+        
+    }
 
-        }
+    function hideoneClass(e) {
+        e.preventDefault();
+        $('.first-element-image').hide();
+        $('.two-element-image').hide();
+        $('.elements-image').show();
     }
 });
 
