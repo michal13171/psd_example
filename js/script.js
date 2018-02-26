9fd1759e3d7109847b41f23d82b7102572d454a7
Michał Kukuła <michal13171@wp.pl>
2018-02-26 20:08:30 +0100

    ini

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 "use strict"; 
-import { watchimage } from "./functionsImage";
 
 $(function() {
    $(".three").on("click", watchimage_second);
@@ -11,10 +10,6 @@ $(function() {
   var video = document.getElementById("video");
   var progressBar = document.querySelector(".progressBar");
 
-  module1.watchimage();
-  module1.watchimage_second();
-  module.w
-
   function hideoneClass(e) {
     e.preventDefault();
     $(".elements-image").show();
@@ -24,7 +19,94 @@ $(function() {
       one.hide();
     }
   }
+       function watchimage(e) {
+         e.preventDefault();
+         var classTab = ["two-element-image"];
+         var container = ["first-element-image"];
+         var imageTab = ["img/portfoliophoto6.png", "img/portfoliophoto3.png", "img/portfolio%20photo.png", "img/portfoliophoto1.png"];
+         var imageTabSecond = ["img/portfoliophoto4.png", "img/portfoliophoto5.png", "img/portfoliophoto7.png", "img/portfolio%20photo2.png"];
 
+         $(".elements-image").hide();
+         $(".first-element-image")
+           .html("")
+           .show()
+           .css({
+             display: "flex",
+             "justify-content": "space-between",
+             "align-items": "center"
+           });
+         $(".two-element-image")
+           .html("")
+           .show()
+           .css({
+             display: "flex",
+             "justify-content": "space-between",
+             "align-items": "center"
+           });
+
+         jQuery.each(container, function(i, val) {
+           $.each(imageTab, function(j, valum) {
+             var valueImage = document.createElement("img");
+             valueImage.src = valum;
+             var cont = $("." + val);
+             cont.append(valueImage);
+           });
+         });
+
+         jQuery.each(classTab, function(i, val) {
+           $.each(imageTabSecond, function(j, valum) {
+             var valueImage = document.createElement("img");
+             valueImage.src = valum;
+             var cont = $("." + val);
+             cont.append(valueImage);
+             console.log(val);
+           });
+         });
+       }
+
+       function watchimage_second(e) {
+         e.preventDefault();
+         var container = ["first-element-image"];
+         var classTab = ["two-element-image"];
+         var imageTab = ["img/portfoliophoto6.png", "img/portfoliophoto3.png", "img/portfolio%20photo.png", "img/portfoliophoto1.png"];
+         var imageTabSecond = ["img/img_one (1).jpeg", "img/img_one (1).png", "img/img_one (2).jpeg", "img/img_one (2).png"];
+
+         $(".elements-image").hide();
+         $(".first-element-image")
+           .html("")
+           .show()
+           .css({
+             display: "flex",
+             "justify-content": "space-between",
+             "align-items": "center"
+           });
+         $(".two-element-image")
+           .html("")
+           .show()
+           .css({
+             display: "flex",
+             "justify-content": "space-between",
+             "align-items": "center"
+           });
+
+         jQuery.each(container, function(i, val_one) {
+           $.each(imageTab, function(j, value_one) {
+             var valueImage_one = document.createElement("img");
+             valueImage_one.src = value_one;
+             var contain = $("." + val_one);
+             contain.append(valueImage_one);
+           });
+         });
+
+         jQuery.each(classTab, function(i, val) {
+           $.each(imageTabSecond, function(j, valum) {
+             var valueImage = document.createElement("img");
+             valueImage.src = valum;
+             var cont = $("." + val);
+             cont.append(valueImage);
+           });
+         });
+       }
   function play_pause(e) {
     e.preventDefault();
     if (video.paused) {
